8597ac49355ba21c064040d9b75950509b9588e6
Jhon Chiguay <informaticojhon@gmail.com>
2024-10-01 14:39:29 -0300

    Proyecto Iniciado

---
+++ src/firebaseconfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2dtE2MVtsLTRo4ywu74-YKwHpQtp3iQ0",
+  authDomain: "fir-2-80086.firebaseapp.com",
+  projectId: "fir-2-80086",
+  storageBucket: "fir-2-80086.appspot.com",
+  messagingSenderId: "324172285404",
+  appId: "1:324172285404:web:853294cb993649d3b7fb19"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+
+<template>
+  <main class="home-container">
+    <h1>Bienvenido al Home! 😎</h1>
+    <h2>Has iniciado sesión 🤭!</h2>
+    <a
+      ><img
+        class="catgif"
+        src="https://i.pinimg.com/originals/a9/fb/d4/a9fbd437bf2255bf60b6bff1fabf4bed.gif"
+        alt=""
+    /></a>
+  </main>
+</template>
+
+<style scoped>
+.home-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f0f0f5;
+  color: #333;
+  font-family: "Arial", sans-serif;
+  padding: 2rem;
+}
+
+h1 {
+  font-size: 3rem;
+  color: #2c3e50;
+}
+
+h2 {
+  font-size: 2rem;
+  color: #2980b9;
+}
+
+.catgif {
+  width: 300px;
+  height: auto;
+  margin: 0 auto;
+}
+</style>
